41b77e6a5250c7fd98afe4b5f5891cc9b996d875
Switly01 <esatorl8581@gmail.com>
2026-08-08 16:01:41 +0300

    Delete session-bootstrap.js

--- session-bootstrap.js
+++
@@ -1,8 +0,0 @@
-try {
-  const storedState = JSON.parse(localStorage.getItem("play-streamers-v17-site") || "{}");
-  if (storedState.settings?.userSession || storedState.userSession) {
-    document.documentElement.classList.add("ps15-session-pending");
-  }
-} catch {
-  // Bozuk yerel veri ilk sayfa çizimini engellemez.
-}
